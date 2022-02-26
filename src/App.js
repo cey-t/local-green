30c1211d893d12b77390027275d354c99a2a1e8d
cyd <ceydatoker2209@gmail.com>
2022-02-26 13:59:20 +0200

    finally

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="http://www.archisacademy.com"
+          href="https://www.archisacademy.com"
           target="_blank"
           rel="noopener noreferrer"
         >
